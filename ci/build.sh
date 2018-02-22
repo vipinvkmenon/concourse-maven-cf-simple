5f72c3d10df2df7b8fd9ca09fb213426cd99d43f
Vipin Menon <vipinvkmenon@users.noreply.github.com>
2018-02-22 10:32:33 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,12 +2,6 @@
 
 set -e -x
 
-#cd source-code
-#  ./mvnw clean package
-#cd ..
-
-#cp source-code/target/concourse-maven-cf-simple-0.0.1-SNAPSHOT.jar  build-output/.
-
 curl ipecho.net/plain ; echo
 
 wget -qO- http://ipecho.net/plain ; echo
